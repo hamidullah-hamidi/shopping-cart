c9d13d8a6f83fc4a2ba1d2f4466e9f279762f49c
Hamidullah <hamidullahh754@gmail.com>
2025-01-29 22:17:04 +0430

    u

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import './App.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import CharacterGrid from './components/characters/CharacterGrid';
-import Header from './components/Header';
-import Search from './components/Search';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState('');
-
-  useEffect(() => {
-    async function fetchItems() {
-      const res = await fetch(`https://rickandmortyapi.com/api/character?name=${query}`);
-      const data = await res.json();
-
-      setItems(data.results);
-      setIsLoading(false);
-
-      console.log(data.results);
-    }
-
-    fetchItems();
-  }, [query]);
-
   return (
-    <div className='container'>
-      <Header />
-      <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid items={items} isLoading={isLoading} />
+    <div>
+      <Navbar />
     </div>
   );
 }
