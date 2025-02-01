f055a5c1712326a1b61c6962658874742e840678
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 14:23:31 +0430

    u

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import { Button, Navbar, Modal, Container, Nav } from 'react-bootstrap';
 
 function NavbarComponent() {
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <Navbar expand='sm'>
         <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className=' justify-content-end'>
-          <Button>Cart 0 Items</Button>
+          <Button onClick={() => setShow(true)}>Cart 0 Items</Button>
         </Navbar.Collapse>
       </Navbar>
-      <Modal>
-        <Modal.Header>Hi Dear</Modal.Header>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>Hi Dear</Modal.Header>
         <Modal.Body>
           <h3>you can choose...</h3>
         </Modal.Body>
