eab5e6998b0a87d770c1c8509c101c0b0bd8c5c2
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 11:46:30 +0430

    u

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,14 @@
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 function Store() {
   return (
     <>
       <h1>Welcome to the Store!</h1>
-      <Row xs={1} md={3} className='g-4'></Row>
+      <Row xs={1} md={3} className='g-4'>
+        <Col align='center'>
+          <h1>Product</h1>
+        </Col>
+      </Row>
     </>
   );
 }
