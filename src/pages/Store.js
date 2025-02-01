b65affa2696784f87a882facbe8cd83d6da40f0d
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 12:20:16 +0430

    u

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,22 +1,16 @@
 import { Col, Row } from 'react-bootstrap';
+import { productsArray } from '../productsStore';
 
 function Store() {
   return (
     <>
       <h1>Welcome to the Store!</h1>
       <Row xs={1} md={4} className='g-4'>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
-        <Col align='center'>
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align='center'>
+            <h2>{product.title}</h2>
+          </Col>
+        ))}
       </Row>
     </>
   );
