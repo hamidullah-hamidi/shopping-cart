08ac85bfc0e4735457322bb21b8120fed23f6fe4
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 23:45:25 +0430

    u

--- src/cartContext.js
+++ src/cartContext.js
@@ -1,11 +1,47 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 // 1)   create context
 const CardContext = createContext();
 
 // 2)   create Provider
-function CartProvider() {
-  
+function CartProvider({ children }) {
+  const [cardProducts, setCardProducts] = useState([]);
+
+  function getProductQuantity(id) {
+    const quantity = cardProducts.find((product) => product.id === id)?.quantity;
+    if (quantity === undefined) return 0;
+
+    return quantity;
+  }
+
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 0) {
+      setCardProducts([...cardProducts, { id: id, quantity: 1 }]);
+    } else {
+      setCardProducts(
+        cardProducts.map((product) =>
+          product.id === id ? { ...product, quantity: product.quantity + 1 } : product
+        )
+      );
+    }
+  }
+
+  return (
+    <CardContext.Provider
+      value={{
+        items: cardProducts,
+        getProductQuantity,
+        addOneToCart,
+        removeOneFromCart,
+        deleteFromCart,
+        getTotalCost,
+      }}
+    >
+      {children}
+    </CardContext.Provider>
+  );
 }
 
 // 3)   export context
