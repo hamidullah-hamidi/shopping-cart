2f16aca627927e704d748c9cd50c69b8d0b62f41
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 10:38:59 +0430

    u

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import NavbarComponent from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
+import Store from './pages/Store';
 
 function App() {
   return (
-    <div>
+    <Container>
       <NavbarComponent />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path='/success' element={<Success />} />
+          <Route path='/cancel' element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ src/pages/Store.js
@@ -0,0 +1,12 @@
+import { Row } from 'react-bootstrap';
+
+function Store() {
+  return (
+    <>
+      <h1>Welcome to the Store!</h1>
+      <Row xs={1} md={3} className='g-4'></Row>
+    </>
+  );
+}
+
+export default Store;
