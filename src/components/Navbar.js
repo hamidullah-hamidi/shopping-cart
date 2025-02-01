c22d14f501a501014ffffbd9dc56011344320881
Hamidullah <hamidullahh754@gmail.com>
2025-02-01 12:56:19 +0430

    create the modal in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,21 @@ import { Button, Navbar, Modal, Container, Nav } from 'react-bootstrap';
 
 function NavbarComponent() {
   return (
-    <Navbar expand='sm'>
-      <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className=' justify-content-end'>
-        <Button>Cart 0 Items</Button>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar expand='sm'>
+        <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className=' justify-content-end'>
+          <Button>Cart 0 Items</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal>
+        <Modal.Header>Hi Dear</Modal.Header>
+        <Modal.Body>
+          <h3>you can choose...</h3>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
 
